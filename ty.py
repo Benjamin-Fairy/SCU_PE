886043e29c6eafd42a440a539b724e1b30ee3c32
Izumi Sucrose <facionia@vip.qq.com>
2025-11-12 13:50:29 +0800

    Remove debug print statements from ty.py
    Removed print statements for right answers in question parsing.

--- ty.py
+++ ty.py
@@ -89,11 +89,9 @@ def htmlAnalys(htmlFile: str) -> dict:
         types=i.xpath("span/text()")[0].replace("[","").replace("]","")
         if answers: # 选择题
             right=i.xpath("text()")
-            print(right)
             clean_r = re.findall(r'\t\t\t\t\t\t  ([A-Z]+)\n\t\t\t\t\t', str(right[-1]))[0]
         else:   # 判断题
             right = i.xpath("text()")
-            print(right)
             clean_r = re.findall(r'\t\t\t\t\t\t  (.{2})\t\t\t\t\t', str(right[-1]))[0]
 
         ans[types][question]={"content":answers, "answer": clean_r}
